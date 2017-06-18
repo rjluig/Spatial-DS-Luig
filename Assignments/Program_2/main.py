f398032d3cdbe684b04fc8430300fc2a2daa43c0
rjluig <rjluig@gmail.com>
2017-06-18 14:34:36 -0500

    part 1 is finsihed

--- Assignments/Program_2/main.py
+++ Assignments/Program_2/main.py
@@ -60,55 +60,60 @@ def clean_area(screen,origin,width,height,color):
     pygame.draw.polygon(screen, color, points, 0)
 
 #====================================================================================================
+class Map(object):
+    def __init__(self):
+        self.xmin = 999999999
+        self.xmax = -1
 
-def adjust_points(pointslst, width, height):
-    newPoints = []
-    xmin = 999999999
-    xmax = -1
+        self.ymin = 999999999
+        self.ymax = -1
 
-    ymin = 999999999
-    ymax = -1
+    def adjust_points(self, pointslst):
+        newPoints = []
 
-    for x, y in pointslst:
+        for x, y in pointslst:
+            if(x != ''):
+                newx = int((x - self.xmin) / (self.xmax - self.xmin) * 1000)
+                newy = int((1 - (y - self.ymin)/(self.ymax - self.ymin)) * 1000)
+                newPoints.append((newx, newy))
 
-        if(x != ''):
-            x = int(x)
-            y = int(y)
-            if xmin > x:
-                xmin =x
-            elif xmax < x:
-                xmax = x
+        return newPoints
 
-            if ymin > y:
-                ymin = y
-            elif ymax < y:
-                ymax = y
+    def extract_points(self, crimelist):
+        x = -1
+        y = -1
+        points = []
+        for line in crimelist:
+            x = line[19]
+            y = line[20] 
+            if(x != ''):
+                x = int(x)
+                y = int(y)
+                if self.xmin > x:
+                    self.xmin =x
+                elif self.xmax < x:
+                    self.xmax = x
 
-    for x, y in pointslst:
-        if(x != ''):
-            x = int(x)
-            y = int(y)
-            newx = int(((x - xmin) / (xmax - xmin)) * width)
-            newy = int(((y - ymin)/(ymax - ymin)) * height)
-            newPoints.append((newx, newy))
+                if self.ymin > y:
+                    self.ymin = y
+                elif self.ymax < y:
+                    self.ymax = y
+                points.append((x,y))
 
-    return newPoints
-
-def extract_points(crimelist):
-    x = -1
-    y = -1
-    points = []
-    for line in crimelist:
-        x = line[19]
-        y = line[20]
-        points.append((x,y))
-
-    return points
+        return points
 
 if __name__ == '__main__':
+    DIRPATH = os.path.dirname(os.path.realpath(__file__))
+
     background_colour = (255,255,255)
-    black = (0,0,0)
-    (width, height) = (600, 400)
+    BLACK = (0,0,0)
+    BLUE = (0,0,255)
+    RED  =(255,0,0)
+    GREEN = (0,255,0)
+    YELLOW = (255,255,0)
+    PURPLE = (128,0,128)
+
+    (width, height) = (1000, 1000)
 
     screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption('Simple Line')
@@ -116,26 +121,46 @@ if __name__ == '__main__':
 
     pygame.display.flip()
 
+    map = Map()
+
     epsilon = 10
     min_pts = 4.0
+    t = 'filtered_'
+    c = 'crimes_'
+    pngName = '_screen_shot.png'
+    extention = '.csv'
+    keys = ['bronx', 'brooklyn', 'manhattan', 'queens', 'staten_island']
 
-    points = []
+    colour ={'manhattan':(194,35,38), 'queens':(243,115,56), 'staten_island':(253,182,50), \
+        'bronx':(2,120,120), 'brooklyn':(128,22,56) }
+    images = []
+    boroughs = {}
 
-    
-    points = read_crime_data.get_crime_data()
-    points = extract_points(points)
-    points = adjust_points(points, width, height)
-    
+    for f in keys:
+        points = []
+        points = read_crime_data.get_crime_data(t+c+f+extention)
+        points = map.extract_points(points)
+        #print(points)
+        boroughs[f] = points
 
-    mbrs = calculate_mbrs(points, epsilon, min_pts)
+    for key in boroughs:
+        boroughs[key] = map.adjust_points(boroughs[key])
+        for i in range(len(boroughs[key])):
+            
+            pygame.draw.circle(screen, colour[key], boroughs[key][i], 1, 0)
+        #print(DIRPATH)
+    pygame.image.save(screen, DIRPATH + '/' + 'all_buroughs_screen_shot.png' )
+    screen.fill(background_colour)
 
-    running = True
+    # mbrs = calculate_mbrs(points, epsilon, min_pts)
+    screenshot = pygame.image.load(DIRPATH + '/' + 'all_buroughs_screen_shot.png' )
+    screen.blit(screenshot, (0,0))
+    running = True  
     while running:
 
-        for p in points:
-            pygame.draw.circle(screen, black, p, 3, 0)
-        for mbr in mbrs:
-            pygame.draw.polygon(screen, black, mbr, 2)
+        
+        # for mbr in mbrs:
+        #     pygame.draw.polygon(screen, black, mbr, 2)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
--- Assignments/Program_2/read_crime_data.py
+++ Assignments/Program_2/read_crime_data.py
@@ -2,20 +2,19 @@ import pprint as pp
 import os,sys
 
 # Get current working path
-def get_crime_data():
+def get_crime_data(FileName):
     DIRPATH = os.path.dirname(os.path.realpath(__file__))
     keys = []
     crimes = []
 
     got_keys = False
     #with open(DIRPATH+'/../NYPD_CrimeData/Nypd_Crime_01') as f:
-    with open(DIRPATH+'/'+'nypd_small_data.txt') as f:
+    with open(DIRPATH+'/'+FileName) as f:
         for line in f:
             line = ''.join(x if i % 2 == 0 else x.replace(',', ':') for i, x in enumerate(line.split('"')))
             line = line.strip().split(',')
             if not got_keys:
                 keys = line
-                print(keys)
                 got_keys = True
                 continue
 
